e1bf58e357e1c40c52a9a06914c194ccbfbb7298
AISYAH SITI ZAHRA <160479611+aszahra@users.noreply.github.com>
2025-03-21 14:03:15 +0700

    add food pages

--- lib/main.dart
+++ lib/main.dart
@@ -74,7 +74,7 @@ class FoodShopMain extends StatelessWidget {
 
                   switch(settings.name) {
                     case '/main':
-                      page = Center(child: Text('main'));
+                      page = FoodMainPage();
                       break;
                     case '/favorites':
                       page = Center(child: Text('favorites'));
@@ -172,6 +172,112 @@ class FoodBottomBarSelectionService extends ChangeNotifier {
   }
 }
 
+class FoodMainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        FoodPager()
+      ],
+    );
+  }
+}
+
+class FoodPager extends StatefulWidget {
+  @override
+  State<FoodPager> createState() => _FoodPagerState();
+}
+
+class _FoodPagerState extends State<FoodPager> {
+
+  List<FoodPage> pages = [
+    FoodPage(imgUrl: Utils.foodPromo1, logoImgUrl: 'assets/text-white.png'),
+    FoodPage(imgUrl: Utils.foodPromo2, logoImgUrl: 'assets/text-white.png'),
+    FoodPage(imgUrl: Utils.foodPromo3, logoImgUrl: 'assets/text-white.png'),
+  ];
+
+  int currentPage = 0;
+  PageController? controller;
+
+  @override
+  void initState() {
+    controller = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 350,
+      child: Column(
+        children: [
+          Expanded(
+            child: PageView(
+              scrollDirection: Axis.horizontal,
+              pageSnapping: true,
+              controller: controller,
+              children: List.generate(pages.length, (index) {
+                FoodPage currentPage = pages[index];
+
+                return Container(
+                  alignment: Alignment.bottomLeft,
+                  margin: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        blurRadius: 10,
+                        offset: Offset(0.0, 5.0),
+                      ),
+                    ],
+                    image: DecorationImage(
+                      image: NetworkImage(currentPage.imgUrl!),
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.5), // Efek gelap
+                        BlendMode.darken,
+                      ),
+                    ),
+                  ),
+                  child: Image.network(currentPage.logoImgUrl!, width: 120),
+                );
+              }),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FoodPage extends StatelessWidget {
+  final String imgUrl;
+  final String logoImgUrl;
+
+  FoodPage({required this.imgUrl, required this.logoImgUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ColorFiltered(
+          colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken),
+          child: Image.asset(imgUrl, fit: BoxFit.cover),
+        ),
+        Center(child: Image.asset(logoImgUrl)), // Teks/logo di atas
+      ],
+    );
+  }
+}
+
 class Utils {
   static GlobalKey<NavigatorState> mainListNav = GlobalKey();
   static GlobalKey<NavigatorState> mainAppNav = GlobalKey();
@@ -182,7 +288,7 @@ class Utils {
   static const String donutLogoRedText = 'https://romanejaquez.github.io/flutter-codelab4/assets/donut_shop_text.png';
   static const String donutTitleFavorites = 'https://romanejaquez.github.io/flutter-codelab4/assets/donut_favorites_title.png';
   static const String donutTitleMyDonuts = 'https://romanejaquez.github.io/flutter-codelab4/assets/donut_mydonuts_title.png';
-  static const String donutPromo1 = 'https://romanejaquez.github.io/flutter-codelab4/assets/donut_promo1.png';
-  static const String donutPromo2 = 'https://romanejaquez.github.io/flutter-codelab4/assets/donut_promo2.png';
-  static const String donutPromo3 = 'https://romanejaquez.github.io/flutter-codelab4/assets/donut_promo3.png';
+  static const String foodPromo1 = 'assets/foodpromo2.jpg';
+  static const String foodPromo2 = 'assets/foodpromo1.webp';
+  static const String foodPromo3 = 'assets/foodpromo3.jpg';
 }
