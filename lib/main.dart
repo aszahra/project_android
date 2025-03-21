d8fc0a48d2ceaefd9a816a19c7a7c7855fb0def9
AISYAH SITI ZAHRA <160479611+aszahra@users.noreply.github.com>
2025-03-21 14:20:07 +0700

    add pageviewindicator

--- lib/main.dart
+++ lib/main.dart
@@ -222,6 +222,11 @@ class _FoodPagerState extends State<FoodPager> {
               scrollDirection: Axis.horizontal,
               pageSnapping: true,
               controller: controller,
+              onPageChanged: (int page) {
+                setState(() {
+                  currentPage = page;
+                });
+              },
               children: List.generate(pages.length, (index) {
                 FoodPage currentPage = pages[index];
 
@@ -252,6 +257,11 @@ class _FoodPagerState extends State<FoodPager> {
               }),
             ),
           ),
+          PageViewIndicator(
+            controller: controller,
+            numberOfPages: pages.length,
+            currentPage: currentPage,
+          )
         ],
       ),
     );
@@ -274,6 +284,43 @@ class FoodPage extends StatelessWidget {
         ),
         Center(child: Image.asset(logoImgUrl)), // Teks/logo di atas
       ],
+    );
+  }
+}
+
+class PageViewIndicator extends StatelessWidget {
+  PageController? controller;
+  int? numberOfPages;
+  int? currentPage;
+
+  PageViewIndicator({ this.controller, this.numberOfPages, this.currentPage });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(numberOfPages!, (index) {
+        return GestureDetector(
+          onTap: () {
+            controller!.animateToPage(
+              index,
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.easeInOut);
+          },
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 250),
+            curve: Curves.easeInOut,
+            width: 15,
+            height: 15,
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: currentPage == index ?
+                Utils.mainColor : Colors.grey.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(10)
+            )
+          )
+        );
+      })
     );
   }
 }
